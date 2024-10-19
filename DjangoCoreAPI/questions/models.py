b9d301ffd2ea82f39445c251532bd8c19c9d34a4
Furkan Yamak <furkan61400@gmail.com>
2024-10-19 18:45:35 +0300

    questions init

---
+++ DjangoCoreAPI/questions/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings  # To reference the User model
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions')
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name} - {self.question.title}'
