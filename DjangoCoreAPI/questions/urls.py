b5720ac390f3f08b918b9af151decc7f55465d2f
Furkan Yamak <furkan61400@gmail.com>
2024-11-26 03:22:27 +0300

    Add | user's own questions

--- DjangoCoreAPI/questions/urls.py
+++ DjangoCoreAPI/questions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from questions.views import (
-    ListQuestionView, CreateQuestion, CreateComment, 
+    ListQuestionView, CreateQuestion, CreateComment, OwnQuestions,
     LikeQuestionView, DislikeQuestionView, LikeCommentView, DislikeCommentView,
     FavoriteQuestionView,
 )
@@ -10,6 +10,7 @@ urlpatterns = [
     path("list-questions/", ListQuestionView.as_view(), name="list_question"),
     path("create-question/",CreateQuestion.as_view(),name="create_question"),
     path("create-comment/",CreateComment.as_view(),name="create_comment"),
+    path("own-questions/",OwnQuestions.as_view(),name="own_questions"),
     path("like-question/<int:pk>", LikeQuestionView.as_view(), name="like_question"),
     path("dislike-question/<int:pk>", DislikeQuestionView.as_view(), name="dislike_question"),
     path("like-comment/<int:pk>", LikeCommentView.as_view(), name="like_comment"),
--- DjangoCoreAPI/questions/views.py
+++ DjangoCoreAPI/questions/views.py
@@ -51,7 +51,19 @@ class CreateComment(APIView):
             serializer.save(user = request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
-            
+      
+
+class OwnQuestions(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(
+        responses={200: QuestionSerializer()}
+    )
+    def get(self,request):
+        own_questions = Question.objects.filter(user=request.user)
+        serializer = QuestionSerializer(own_questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+      
     
 class LikeQuestionView(APIView):
     permission_classes = [IsAuthenticated]
